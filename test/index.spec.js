5615a3e4c553ecc25b002a891de8d1fbc5bb7a29
zinai <313670398L@gmail.com>
2020-03-08 11:15:17 +0800

    fix: add travis_wait cause of 10min timeout

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,18 +3,18 @@ const addon = require("..");
 describe("basic", () => {
   it("works with async/await", async () => {
     expect.hasAssertions();
-    const data = await addon.checkEmailExists("toEmail@gmail.com");
+    const data = await addon.checkEmailExists("someone@gmail.com");
     expect(typeof data).toEqual("object");
     expect(data.smtp.is_deliverable).toEqual(false);
   });
 
   it("should return a JSON obj", () => {
-    const data = addon.checkEmailExistsSync("toEmail@gmail.com");
+    const data = addon.checkEmailExistsSync("someone@gmail.com");
     expect(typeof data).toEqual("object");
   });
 
   it("example email verified fail", () => {
-    const data = addon.checkEmailExistsSync("toEmail@gmail.com");
+    const data = addon.checkEmailExistsSync("someone@gmail.com");
     expect(data.smtp.is_deliverable).toEqual(false);
   });
 
