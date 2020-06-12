9507ba6d49c355f532e058e9c5c7148e0b5d0754
zinai <313670398L@gmail.com>
2020-06-13 01:44:50 +0800

    feat: opz deps and test

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,17 +5,17 @@ describe("basic", () => {
   // this two case can`t pass in travis-ci`s linux os and coast too much time,
   // but quickly passed in oxs env.
 
-  // it("works with async/await", async () => {
-  //   expect.hasAssertions();
-  //   const data = await addon.checkEmailExists("someone@gmail.com");
-  //   expect(typeof data).toEqual("object");
-  //   expect(data.smtp.is_deliverable).toEqual(false);
-  // });
+  it("works with async/await", async () => {
+    // expect.hasAssertions(2);
+    const data = await addon.checkEmailExists("someone@gmail.com");
+    expect(typeof data).toEqual("object");
+    expect(data.smtp.is_deliverable).toEqual(false);
+  });
 
-  // it("example email verified fail", () => {
-  //   const data = addon.checkEmailExistsSync("someone@gmail.com");
-  //   expect(data.smtp.is_deliverable).toEqual(false);
-  // });
+  it("example email verified fail", () => {
+    const data = addon.checkEmailExistsSync("someone@gmail.com");
+    expect(data.smtp.is_deliverable).toEqual(false);
+  });
 
   it("should return a JSON obj", () => {
     const data = addon.checkEmailExistsSync("someone@gmail.com");
